3a30bddfcf2869966b326661cea759c0aa726d9a
mohamedbesrour <m.besrour@yahoo.com>
2024-05-19 14:11:43 +0200

    ajout init.sql

--- README.md
+++ README.md
@@ -9,8 +9,20 @@ docker-compose --version
 docker-compose up
 docker-compose up --build
 
+docker ps   -- pour consulter les ID images docker
+
+pour connaitre l'IP
+docker container ls
+docker inspect 24006bb24900 
 "IPAddress": "172.18.0.3",
 
   postgres:
     volumes:
       - ./pgdata:/var/lib/postgres/data
+
+
+docker exec -it 2b09b1d35e5a bash       id de etna-node
+apt-get install netcat -y
+root@2b09b1d35e5a:/server# nc -zv postgres 5432
+DNS fwd/rev mismatch: postgres != postgres.etna_default
+postgres [172.18.0.3] 5432 (postgresql) open
---
+++ init_db/init.sql
@@ -0,0 +1,76 @@
+CREATE DATABASE velo;
+
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    role VARCHAR(255)
+);
+
+CREATE TABLE utilisateurs (
+    id_utilisateur SERIAL PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL CHECK (role IN ('admin', 'user'))
+);
+-- INSERT INTO utilisateurs (nom, prenom, email, password, role) VALUES ('besrour', 'mohamed', 'besrou_m@etna-alternance.net', 'root', user);
+
+-- INSERT INTO produits (titre, categorie, description, image, prix, stock) VALUES ('VTT tout terrain', 'VTT', 'Vélo tout terrain robuste pour les aventures en plein air.', 'https://product-cdn-frz.alltricks.com/large/284/562284/2562284/5168255', 180, 4);
+CREATE TABLE produits (
+    id_produit SERIAL PRIMARY KEY,
+    titre VARCHAR(255) NOT NULL,
+    categorie VARCHAR(255),  
+    description VARCHAR(255),
+    image VARCHAR(999),
+    prix DECIMAL(10, 2) NOT NULL,
+    stock INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE commandes (
+    id_commande SERIAL PRIMARY KEY,
+    id_utilisateur INTEGER REFERENCES utilisateurs(id_utilisateur),
+    date_commande TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    statut_commande VARCHAR(255)
+);
+
+CREATE TABLE details_commande (
+    id_detail_commande SERIAL PRIMARY KEY,
+    id_commande INTEGER REFERENCES commandes(id_commande),
+    id_produit INTEGER REFERENCES produits(id_produit),
+    quantite INTEGER,
+    prix_unitaire DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE historique_commande (
+    id_historique SERIAL PRIMARY KEY,
+    id_utilisateur INTEGER REFERENCES utilisateurs(id_utilisateur),
+    id_commande INTEGER REFERENCES commandes(id_commande),
+    date_commande TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--Fonction nécessaire pour pallier au ON UPDATE CURRENT_TIMESTAMP et mettre a jour update_at
+CREATE OR REPLACE FUNCTION maj_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_maj_updated_at
+BEFORE UPDATE ON produits
+FOR EACH ROW EXECUTE FUNCTION maj_updated_at();
+
+-- CREATE TABLE Panier (
+--     id INT PRIMARY KEY,
+--     userId INT,
+--     productId INT,
+--     quantity INT,
+--     FOREIGN KEY (userId) REFERENCES Utilisateur(id),
+--     FOREIGN KEY (productId) REFERENCES Produit(id)
+-- );
